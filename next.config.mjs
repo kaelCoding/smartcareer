8547700b5bd6d3b1e0719d9c343097ec7446073a
cong <you@example.com>
2026-06-30 10:25:38 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'build',
+  basePath: '/smartcareer', // <-- THÊM DÒNG NÀY BẮT BUỘC
+  
   typescript: {
     ignoreBuildErrors: true,
   },
